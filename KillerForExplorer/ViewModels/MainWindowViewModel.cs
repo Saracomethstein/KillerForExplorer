576ffc0c88d6f84f428570526848d15c9edf4bf6
Saracomethstein <adel.sevastianov@yandex.ru>
2023-09-29 21:14:33 +0300

    First commit

---
+++ KillerForExplorer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using KillerForExplorer.Models;
+
+namespace KillerForExplorer.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _buttonText;
+        private RelayCommand _killerExplorerCommand;
+        private RelayCommand _startExplorerCommand;
+
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set
+            {
+                _buttonText = value;
+                NotifyPropertyChanged(nameof(ButtonText));
+            }
+        }
+
+        public RelayCommand KillerExplorerCommand
+            => _killerExplorerCommand ?? (_killerExplorerCommand = new RelayCommand(() =>
+            {
+
+            }));
+
+        public RelayCommand StartExplorerCommand
+            => _killerExplorerCommand ?? (_startExplorerCommand = new RelayCommand(() =>
+            {
+
+            }));
+    }
+}
