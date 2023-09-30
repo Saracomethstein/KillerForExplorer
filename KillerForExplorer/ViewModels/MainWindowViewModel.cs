d507cdc06b1ee4f95b1ca5cef2fc047acaa84bd9
Saracomethstein <adel.sevastianov@yandex.ru>
2023-09-30 19:35:38 +0300

    Fix bugs

--- KillerForExplorer/ViewModels/MainWindowViewModel.cs
+++ KillerForExplorer/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using KillerForExplorer.Models;
+using System;
 using System.Diagnostics;
 using System.Windows;
 
@@ -71,13 +72,39 @@ namespace KillerForExplorer.ViewModels
         private void MiniToTray()
         {
             TrayManager trayManager = new TrayManager(Application.Current.MainWindow as MainWindow);
-            trayManager.MinimizeToTray();
+            if(trayManager != null)
+            {
+                trayManager.MinimizeToTray();
+            }
         }
 
         private void ExitApploication()
         {
-            Process.Start(@"C:\Windows\explorer");
+            string processName = "explorer";
+
+            if (IsProcessRunning(processName) == false)
+            {
+                StartProcess();
+            }
             Process.GetCurrentProcess().Kill();
+        }
+
+        private bool IsProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length > 0;
+        }
+
+        private void StartProcess()
+        {
+            try
+            {
+                Process.Start(@"C:\Windows\explorer");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при запуске процесса: {ex.Message}");
+            }
         }
 
         private void MiniApplication()
